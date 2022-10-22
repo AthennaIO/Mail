72897e4fd97c6b3de438735d39e77f8877624d48
Victor Tesoura Júnior <txsoura@yahoo.com>
2022-10-22 15:19:45 -0300

    initial commit

--- tests/Unit/TemplateTest.js
+++ tests/Unit/TemplateTest.js
@@ -8,7 +8,6 @@
  */
 
 import { test } from '@japa/runner'
-import { Bootstrap } from '#src/index'
 
 test.group('TemplateTest', group => {
   group.setup(() => {})
@@ -16,7 +15,5 @@ test.group('TemplateTest', group => {
   group.teardown(() => {})
   group.each.teardown(() => {})
 
-  test('should be able to create and run tests with this template', async ({ assert }) => {
-    assert.equal(Bootstrap.main('Hello', 'World!'), 'Bootstrap: Hello World!')
-  }).tags(['@mail'])
+  test('should be able to create and run tests with this template', async () => {}).tags(['@mail'])
 })
