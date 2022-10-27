05718486620d42fdd28f9aafb6b9575c88034da6
João Lenon <lenonSec7@gmail.com>
2022-10-27 20:49:06 -0300

    chore(mail): implement mail class method calls to driver

--- src/index.js
+++ src/index.js
@@ -72,7 +72,9 @@ export class Mail {
    *
    * @return {Promise<void>}
    */
-  send() {}
+  async send() {
+    return this.#driver.send()
+  }
 
   /**
    * Define mail sender.
@@ -101,86 +103,146 @@ export class Mail {
   /**
    * Define mail subject.
    *
+   * @param {string} subject
    * @return {Mail}
    */
-  subject() {}
+  subject(subject) {
+    this.#driver.subject(subject)
+
+    return this
+  }
 
   /**
    * Define mail cc.
    *
+   * @param {string} cc
    * @return {Mail}
    */
-  cc() {}
+  cc(...cc) {
+    this.#driver.cc(...cc)
+
+    return this
+  }
 
   /**
    * Define mail bcc.
    *
+   * @param {string} bcc
    * @return {Mail}
    */
-  bcc() {}
+  bcc(...bcc) {
+    this.#driver.bcc(...bcc)
+
+    return this
+  }
 
   /**
    * Define mail reply to.
    *
+   * @param {string} replyTo
    * @return {Mail}
    */
-  replyTo() {}
+  replyTo(replyTo) {
+    this.#driver.replyTo(replyTo)
+
+    return this
+  }
 
   /**
    * Define mail in reply to.
    *
+   * @param {string} inReplyTo
    * @return {Mail}
    */
-  inReplyTo() {}
+  inReplyTo(inReplyTo) {
+    this.#driver.inReplyTo(inReplyTo)
+
+    return this
+  }
 
   /**
    * Define mail references.
    *
+   * @param {string} references
    * @return {Mail}
    */
-  references() {}
+  references(references) {
+    this.#driver.references(references)
+
+    return this
+  }
 
   /**
    * Define mail envelope.
    *
+   * @param {string} envelope
    * @return {Mail}
    */
-  envelope() {}
+  envelope(envelope) {
+    this.#driver.envelope(envelope)
+
+    return this
+  }
 
   /**
    * Define mail attachment.
    *
+   * @param {string} path
    * @return {Mail}
    */
-  attachment() {}
+  attachment(path) {
+    this.#driver.attachment(path)
+
+    return this
+  }
 
   /**
    * Define mail attachments.
    *
+   * @param {string} folderPath
    * @return {Mail}
    */
-  attachments() {}
+  attachments(folderPath) {
+    this.#driver.attachments(folderPath)
+
+    return this
+  }
 
   /**
    * Define mail plain text.
    *
+   * @param {string} text
    * @return {Mail}
    */
-  text() {}
+  text(text) {
+    this.#driver.text(text)
+
+    return this
+  }
 
   /**
    * Define mail html.
    *
+   * @param {string} html
    * @return {Mail}
    */
-  html() {}
+  html(html) {
+    this.#driver.html(html)
+
+    return this
+  }
 
   /**
    * Define mail markdown.
    *
+   * @param {string} markdown
    * @return {Mail}
    */
-  markdown() {}
+  markdown(markdown) {
+    this.#driver.markdown(markdown)
+
+    return this
+  }
 
   /**
    * Define mail view.
