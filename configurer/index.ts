b4e850b5e441fee801c957d18f38dd066c4922ed
jlenon7 <lenonSec7@gmail.com>
2023-12-09 15:53:44 +0000

    refactor(configurer): use addPromise method

--- configurer/index.ts
+++ configurer/index.ts
@@ -11,69 +11,31 @@ import { File } from '@athenna/common'
 import { BaseConfigurer } from '@athenna/artisan'
 
 export default class MailConfigurer extends BaseConfigurer {
-  private configPath = null
-
   public async configure() {
-    const relativePath =
-      (await this.prompt.input(
-        `Where do you want to save the config files? Pressing enter will set the path to ${this.paint.cyan(
-          './config'
-        )}`
-      )) || './config'
-
-    this.configPath = Path.pwd(relativePath)
-
     await this.logger
       .task()
-      .add(`Create mail.${Path.ext()} config file`, t =>
-        this.setTask(t, () => this.taskOne())
-      )
-      .add('Update providers of .athennarc.json', t =>
-        this.setTask(t, () => this.taskTwo())
-      )
-      .add('Update .env, .env.test and .env.example', t =>
-        this.setTask(t, () => this.taskThree())
-      )
+      .addPromise(`Create mail.${Path.ext()} config file`, () => {
+        return new File('./mail').copy(Path.config(`mail.${Path.ext()}`))
+      })
+      .addPromise('Update providers of .athennarc.json', () => {
+        return this.rc
+          .pushTo('providers', '@athenna/mail/providers/MailProvider')
+          .pushTo('providers', '@athenna/mail/providers/SmtpServerProvider')
+          .save()
+      })
+      .addPromise('Update .env, .env.test and .env.example', () => {
+        const envs =
+          '\nMAIL_MAILER=smtp\n' +
+          'MAIL_HOST=localhost\n' +
+          'MAIL_PORT=5025\n' +
+          'MAIL_USERNAME=\n' +
+          'MAIL_PASSWORD=\n'
+
+        return new File(Path.pwd('.env'), '')
+          .append(envs)
+          .then(() => new File(Path.pwd('.env.test'), '').append(envs))
+          .then(() => new File(Path.pwd('.env.example'), '').append(envs))
+      })
       .run()
-  }
-
-  private async taskOne() {
-    let path = './config/mail.js'
-
-    if (Env('IS_TS', false) === true) {
-      path = './config/mail.ts'
-    }
-
-    await new File(path).copy(this.configPath.concat(`/mail.${Path.ext()}`))
-  }
-
-  private async taskTwo() {
-    return this.rc
-      .pushTo('providers', '@athenna/mail/providers/MailProvider')
-      .pushTo('providers', '@athenna/mail/providers/SmtpServerProvider')
-      .save()
-  }
-
-  private async taskThree() {
-    const envs =
-      '\nMAIL_MAILER=smtp\n' +
-      'MAIL_HOST=localhost\n' +
-      'MAIL_PORT=5025\n' +
-      'MAIL_USERNAME=\n' +
-      'MAIL_PASSWORD=\n'
-
-    return new File(Path.pwd('.env'), '')
-      .append(envs)
-      .then(() => new File(Path.pwd('.env.test'), '').append(envs))
-      .then(() => new File(Path.pwd('.env.example'), '').append(envs))
-  }
-
-  private async setTask(task: any, callback: any) {
-    try {
-      await callback()
-      await task.complete()
-    } catch (err) {
-      await task.fail(err)
-    }
   }
 }
